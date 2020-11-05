237ac1d20b39dc21cc951e8e56ea1e5ef76d9853
KyoheiYamamoto <arienkyohei@gmail.com>
2020-11-05 13:54:37 +0900

    記事投稿処理でタグを登録可能にし記事にタグを表示

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -1,54 +1,62 @@
 <template>
   <div>
     <input
-      type="hidden" name="tags" :value="tagsJson">
+      type="hidden"
+      name="tags"
+      :value="tagsJson"
+    >
     <vue-tags-input
       v-model="tag"
       :tags="tags"
+      #----------ここから追加----------
       placeholder="タグを5個まで入力できます"
+      #----------ここまで追加----------
       :autocomplete-items="filteredItems"
       @tags-changed="newTags => tags = newTags"
-    s-changed="newTags => tags = newTags"
     />
   </div>
 </template>
 
 <script>
- import VueTagsInput from '@johmun/vue-tags-input';
-
-export default {
-  components: {
-    VueTagsInput,
-  },
-  data() {
-    return {
-      tag: '',
-      tags: [],
-      autocompleteItems: [{
+  import VueTagsInput from '@johmun/vue-tags-input';
+  export default {
+    components: {
+      VueTagsInput,
+    },
+    props: {
+      initialTags: {
+        type: Array,
+        default: [],
+      },
+    },
+    data() {
+      return {
+        tag: '',
+        tags: this.initialTags,
+        autocompleteItems: [{
           text: 'Spain',
-        },{
+        }, {
           text: 'France',
-        },{
+        }, {
           text: 'USA',
-        },{
+        }, {
           text: 'Germany',
-        },{
+        }, {
           text: 'China',
         }],
-    };
-  },
-    computed:{
-      filteredItems(){
-        return this.autocompleteItems.filter(i => {
-          return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !==-1;
-            });
-        },
-        tagsJson(){
-            return JSON.stringify(this.tags)
-            return JSON.stringify(this.tags)
-        },
+      };
     },
-};
+    computed: {
+      filteredItems() {
+        return this.autocompleteItems.filter(i => {
+          return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
+        });
+      },
+      tagsJson() {
+        return JSON.stringify(this.tags)
+      },
+    },
+  };
 </script>
 <style lang="css" scoped>
   .vue-tags-input {
