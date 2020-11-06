bb16559959c2bdac44787513f0c73a79dcc6ca4c
KyoheiYamamoto <arienkyohei@gmail.com>
2020-11-06 10:11:54 +0900

    フォローボタンを表示

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,11 +10,17 @@
         <div class="d-flex flex-row">
            <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             <i class="fas fa-user circle fa-3x"></i>
-          </a>
+           </a>
+           @if( Auth::id() !== $user->id )
+            <follow-button
+              class="ml-auto"
+            >
+            </follow-button>
+          @endif
         </div>
         <h2 class="h5 card-title m-0">
             <a href="{{route('users.show' ,['name'=>$user->name]) }}" class="text-dark">
-            
+
                 {{$user->name}}
             </a>
         </h2>
