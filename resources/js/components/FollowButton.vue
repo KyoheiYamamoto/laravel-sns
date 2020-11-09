cc8c8ed1e566c4324901392c633b30331eff3034
KyoheiYamamoto <arienkyohei@gmail.com>
2020-11-09 10:23:37 +0900

    フォローボタンのVueコンポーネントからLaravelに非同期通信する

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
-      <button
+    <button
       class="btn-sm shadow-none border border-primary p-2"
       :class="buttonColor"
-      >
+      @click="clickFollow"
+    >
       <i
         class="mr-1"
-         :class="buttonIcon"></i>
-        {{buttonText}}
-      </button>
+        :class="buttonIcon"
+      ></i>
+      {{ buttonText }}
+    </button>
   </div>
-
 </template>
+
 <script>
- export default {
-     props: {
+  export default {
+    props: {
       initialIsFollowedBy: {
         type: Boolean,
         default: false,
       },
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
+      },
     },
-     data() {
+    data() {
       return {
-        isFollowedBy: this.initialIsFollowedBy, //==========この行を変更
+        isFollowedBy: this.initialIsFollowedBy,
       }
     },
     computed: {
@@ -40,6 +49,27 @@
         return this.isFollowedBy
           ? 'フォロー中'
           : 'フォロー'
+      },
+    },
+    methods: {
+      clickFollow() {
+        if (!this.authorized) {
+          alert('フォロー機能はログイン中のみ使用できます')
+          return
+        }
+        this.isFollowedBy
+          ? this.unfollow()
+          : this.follow()
+      },
+      
+      async follow() {
+        const response = await axios.put(this.endpoint)
+        this.isFollowedBy = true
+      },
+
+      async unfollow() {
+        const response = await axios.delete(this.endpoint)
+        this.isFollowedBy = false
       },
     },
   }
